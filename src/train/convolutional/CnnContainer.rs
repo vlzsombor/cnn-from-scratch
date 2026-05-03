2ebeb7a83e65456dcb868b3934a04ac269725093
Zsombor Veres-Lakos <vl.zsombor>
2026-05-03 16:45:29 +0200

    final test + kaggle submission

--- src/train/convolutional/CnnContainer.rs
+++ src/train/convolutional/CnnContainer.rs
@@ -1,28 +1,30 @@
-use ndarray::{s, Array1, Array2, Array3, Array4, ArrayBase, Dim, Ix1, OwnedRepr, Shape};
-use serde::{Deserialize, Serialize};
 use crate::train::convolutional::CnnLayerable::CnnLayerable;
 use crate::train::convolutional::CnnPoolingLayer::CnnPoolingLayer;
 use crate::train::convolutional::CnnSigmoidActivation::CnnSigmoidActivation;
-use crate::train::convolutional::convolutional_matlab::{ConvolutionalMatlab};
 use crate::train::convolutional::ImageData::ImageData;
-use crate::train::layer::{xavier, Activation, ActivationLayer, Layer};
+use crate::train::convolutional::convolutional_matlab::ConvolutionalMatlab;
+use crate::train::layer::{Activation, ActivationLayer, Layer, xavier};
 use crate::train::layer_container::LayerContainer;
 use crate::train::layerable::Layerable;
 use crate::util::ndarray_helper;
 use crate::util::ndarray_helper::xavier2;
+use ndarray::{Array1, Array2, Array3, Array4, ArrayBase, Dim, Ix1, OwnedRepr, Shape, s};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CnnContainer {
     pub layers: Vec<Box<dyn CnnLayerable>>,
     pub linear_container: LayerContainer,
-    image_data: Option<ImageData>
+    image_data: Option<ImageData>,
 }
 
 impl CnnContainer {
-    pub fn new_default() -> Self{
-        let alpha = 0.005;
+    pub fn new_default(learning_rate: f32) -> Self {
+        let alpha = learning_rate;
         let kernel: Array4<f32> = {
-            let mut k = xavier2(&[1,6,5,5]).into_dimensionality::<ndarray::Ix4>().expect("kernel initialization failed");
+            let mut k = xavier2(&[1, 6, 5, 5])
+                .into_dimensionality::<ndarray::Ix4>()
+                .expect("kernel initialization failed");
             // k.slice_mut(s![..,..,1,1]).fill(1.0);
             k
         };
@@ -44,23 +46,29 @@ impl CnnContainer {
         ];
         let linear_layers: Vec<Box<dyn Layerable>> = vec![
             Box::new(Layer::new(192, 10, alpha)),
-//            Box::new(ActivationLayer::sigmoid()),
+            //            Box::new(ActivationLayer::sigmoid()),
         ];
         let linear_container = LayerContainer::new_layers_boxed(linear_layers);
         CnnContainer {
             layers,
             linear_container,
-            image_data: None
+            image_data: None,
         }
     }
     pub fn forward(&mut self, image_data: &ImageData) -> Array2<f32> {
-        let cnn_res = self.layers
+        let cnn_res = self
+            .layers
             .iter_mut()
-            .fold(image_data.clone(), |acc, layer|{
+            .fold(image_data.clone(), |acc, layer| {
                 ImageData::new(layer.forward_propagation(&acc))
             });
         self.image_data = Some(cnn_res.clone());
-        let array2 = cnn_res.image.view().into_shape_with_order((1,cnn_res.image.len())).expect("reshape failed").to_owned();
+        let array2 = cnn_res
+            .image
+            .view()
+            .into_shape_with_order((1, cnn_res.image.len()))
+            .expect("reshape failed")
+            .to_owned();
         let r = self.linear_container.forward(&array2);
         r
     }
@@ -68,50 +76,46 @@ impl CnnContainer {
     pub fn backward_linear(&mut self, delta: Array2<f32>) -> ImageData {
         let back_res = self.linear_container.backward_propagation(delta);
         let image_data = self.image_data.as_ref().unwrap().image.shape();
-        let restored: Array3<f32> = back_res.into_shape_with_order((image_data[0], image_data[1], image_data[2])).unwrap();
+        let restored: Array3<f32> = back_res
+            .into_shape_with_order((image_data[0], image_data[1], image_data[2]))
+            .unwrap();
         self.backward(ImageData::new(restored))
     }
     pub fn backward(&mut self, image_data: ImageData) -> ImageData {
-        let result = self.layers
-            .iter_mut()
-            .rev()
-            .fold(image_data, |acc, layer|{
-                let data = layer.backward_propagation(&acc);
-                ImageData::new(data)
-            });
+        let result = self.layers.iter_mut().rev().fold(image_data, |acc, layer| {
+            let data = layer.backward_propagation(&acc);
+            ImageData::new(data)
+        });
         result
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use ndarray::{array, Array2, Array3};
     use crate::train::convolutional::CnnContainer::CnnContainer;
     use crate::train::convolutional::ImageData::ImageData;
     use crate::train::layer_container::mse_loss_derivative;
     use crate::util::mnist_helper::load_mnist;
+    use ndarray::{Array2, Array3, array};
 
     #[test]
     pub fn test_default_network() {
         let mut sut = CnnContainer::new_default();
         let (x, y) = load_mnist("src/data/mnist_train_small.csv").unwrap();
         let first = x.row(0); //.unwrap();
-        let first = first.mapv(|x| x/255.);
+        let first = first.mapv(|x| x / 255.);
         let first = first.view();
-        let two_dimensional_data: Array2<f32> = first.into_shape_with_order((28, 28))
-            .unwrap()
-            .to_owned();
+        let two_dimensional_data: Array2<f32> =
+            first.into_shape_with_order((28, 28)).unwrap().to_owned();
 
         let image_data: Array3<f32> = first.to_shape((1, 28, 28)).unwrap().to_owned();
         dbg!(&image_data.shape());
-        let image_data_wrapper =ImageData::new(image_data);
+        let image_data_wrapper = ImageData::new(image_data);
 
-        let target: Array2<f32> = array![
-            [0.,0.,0.,0.,0.,0.,1.,0.,0.,0.,]
-        ];
+        let target: Array2<f32> = array![[0., 0., 0., 0., 0., 0., 1., 0., 0., 0.,]];
 
         let mut y_hat = sut.forward(&image_data_wrapper);
-        for _ in 0..100{
+        for _ in 0..100 {
             y_hat = sut.forward(&image_data_wrapper);
             let a = mse_loss_derivative(&y_hat, &target);
             dbg!(&a);
--- src/util/mnist_helper.rs
+++ src/util/mnist_helper.rs
@@ -1,20 +1,43 @@
-﻿use crate::train::layer::{ActivationLayer, Layer};
-use crate::train::layer_container::{cross_entropy_loss_derivative_and_softmax, LayerContainer};
+﻿use crate::train::convolutional::convolutional_layer;
+use crate::train::convolutional::convolutional_layer::ConvolutionalLayer;
+use crate::train::layer::{ActivationLayer, Layer};
+use crate::train::layer_container::{LayerContainer, cross_entropy_loss_derivative_and_softmax};
 use crate::train::loss_functions::softmax;
 use crate::util::util::{accuracy, one_hot};
-use ndarray::{s, Array1, Array2};
+use ndarray::{Array1, Array2, s};
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use crate::train::convolutional::convolutional_layer;
-use crate::train::convolutional::convolutional_layer::ConvolutionalLayer;
+
+pub fn load_mnist_without_label(path: &str) -> Result<Array2<f32>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut X = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let parts: Vec<&str> = line.split(',').collect();
+        let features: Vec<f32> = parts[..].iter().filter_map(|s| s.parse().ok()).collect();
+        X.push(features);
+        //            data.push((features, class.to_string()));
+    }
+
+    let rows = X.len();
+    let cols = X[0].len();
+    let flat: Vec<f32> = X.into_iter().flatten().collect();
+    let x_array2: Array2<f32> = Array2::from_shape_vec((rows, cols), flat).unwrap();
+    Ok(x_array2)
+}
 
 pub fn load_mnist(path: &str) -> Result<(Array2<f32>, Array1<f32>), Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let mut X = Vec::new();
     let mut y = Vec::new();
-    for line in reader.lines(){
+    for line in reader.lines() {
         let line = line?;
         if line.trim().is_empty() {
             continue;
@@ -22,10 +45,7 @@ pub fn load_mnist(path: &str) -> Result<(Array2<f32>, Array1<f32>), Box<dyn Erro
 
         let parts: Vec<&str> = line.split(',').collect();
         let class = parts[0];
-        let features: Vec<f32> = parts[1..]
-            .iter()
-            .filter_map(|s| s.parse().ok())
-            .collect();
+        let features: Vec<f32> = parts[1..].iter().filter_map(|s| s.parse().ok()).collect();
         X.push(features);
         y.push(class.parse::<f32>().unwrap());
         //            data.push((features, class.to_string()));
@@ -42,27 +62,22 @@ pub fn normalize_mnist(x: &Array2<f32>) -> Array2<f32> {
     x / 255.
 }
 
-
-
-pub fn train_mnist_cnn(x: Array2<f32>, y: Array1<f32>) -> Option<f32>
-{
+pub fn train_mnist_cnn(x: Array2<f32>, y: Array1<f32>) -> Option<f32> {
     dbg!(x.raw_dim());
     let subslice = 1;
     let x_train = normalize_mnist(&x).slice(s![..subslice, ..]).to_owned();
     let slice: Vec<usize> = y.iter().map(|&x| x as usize).collect();
     let y_train = one_hot(&slice, 10).slice(s![..subslice, ..]).to_owned();
 
-    let layer = Layer::new(28*28, 128, 0.0001);
+    let layer = Layer::new(28 * 28, 128, 0.0001);
     let aaa = ConvolutionalLayer::new(layer, 28);
-    let layers: Vec<Box<dyn crate::train::layerable::Layerable>> = vec![
-        Box::new(aaa),
-        Box::new(ActivationLayer::relu()),
-    ];
+    let layers: Vec<Box<dyn crate::train::layerable::Layerable>> =
+        vec![Box::new(aaa), Box::new(ActivationLayer::relu())];
     let mut sut = LayerContainer::new_layers_boxed(layers);
-    for i in 0..500{
+    for i in 0..500 {
         let y_hat = sut.forward(&x_train);
         sut.backward_propagation(cross_entropy_loss_derivative_and_softmax(&y_hat, &y_train));
-        if i % 100 == 0  {
+        if i % 100 == 0 {
             let accuracy = accuracy(&y_hat, &y_train);
             dbg!(i, accuracy);
         }
@@ -78,24 +93,23 @@ pub fn train_mnist_cnn(x: Array2<f32>, y: Array1<f32>) -> Option<f32>
     dbg!(X_test.raw_dim(), y_hat.dim());
     Some(accuracy)
 }
-pub fn train_mnist(x: Array2<f32>, y: Array1<f32>) -> Option<f32>
-{
+pub fn train_mnist(x: Array2<f32>, y: Array1<f32>) -> Option<f32> {
     let subslice = 1_000;
     let X_train = normalize_mnist(&x).slice(s![..subslice, ..]).to_owned();
     let slice: Vec<usize> = y.iter().map(|&x| x as usize).collect();
     let y_train = one_hot(&slice, 10).slice(s![..subslice, ..]).to_owned();
     let layers: Vec<Box<dyn crate::train::layerable::Layerable>> = vec![
-        Box::new(Layer::new(28*28, 128, 0.0001)),
+        Box::new(Layer::new(28 * 28, 128, 0.0001)),
         Box::new(ActivationLayer::relu()),
-        Box::new(Layer::new(128, 128,  0.0001)),
+        Box::new(Layer::new(128, 128, 0.0001)),
         Box::new(ActivationLayer::relu()),
         Box::new(Layer::new(128, 10, 0.0001)),
     ];
     let mut sut = LayerContainer::new_layers_boxed(layers);
-    for i in 0..500{
+    for i in 0..500 {
         let y_hat = sut.forward(&X_train);
         sut.backward_propagation(cross_entropy_loss_derivative_and_softmax(&y_hat, &y_train));
-        if i % 100 == 0  {
+        if i % 100 == 0 {
             let accuracy = accuracy(&y_hat, &y_train);
             dbg!(i, accuracy);
         }
@@ -112,9 +126,6 @@ pub fn train_mnist(x: Array2<f32>, y: Array1<f32>) -> Option<f32>
     Some(accuracy)
 }
 
-
-
-
 #[cfg(test)]
 mod tests {
     use crate::util::mnist_helper::{load_mnist, train_mnist};
@@ -123,17 +134,16 @@ mod tests {
     #[test]
     pub fn mnist() {
         let (X, y) = load_mnist("src/data/mnist_train_small.csv").unwrap();
-        let accuracy = train_mnist(X,y).unwrap();
+        let accuracy = train_mnist(X, y).unwrap();
         dbg!(accuracy);
-        assert!(accuracy>0.85)
+        assert!(accuracy > 0.85)
     }
 }
-
 
 use image::{GrayImage, Luma};
 
 pub fn csv_to_image_oned(pixels: &Array1<f32>, path: &str) {
-    let twod = &pixels.clone().into_shape_with_order((28,28)).unwrap();
+    let twod = &pixels.clone().into_shape_with_order((28, 28)).unwrap();
     csv_to_image(twod, path)
 }
 
